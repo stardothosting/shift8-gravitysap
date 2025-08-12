60fee7da178d04f05b03ea55527bc75f3a83e842
SDH <admin@stardothosting.com>
2025-08-12 15:31:25 -0400

    Testing

--- push.sh
+++ push.sh
@@ -2,8 +2,8 @@
 
 if [ $# -eq 0 ]
 then
-	echo "No push argument was provided"
-	exit 1
+    echo "No push argument was provided"
+    exit 1
 fi
 
 # push git first
@@ -12,15 +12,23 @@ git commit -m "$1"
 git push origin main
 
 # rsync to svn
-mkdir ./svn/tags/$2
+# NOTE: removed the mkdir/rsync into tags/$2 to avoid svn cp conflict
 rsync -ravzp --exclude-from './push.exclude' ./ ./svn/trunk
-rsync -ravzp --exclude-from './push.exclude' ./ ./svn/tags/$2
 rsync -ravzp ../wordpress-org-assets/ ./svn/assets
+
 cd svn
 svn --username shift8 add trunk/*
 svn --username shift8 add assets/*
+
+# commit trunk changes
 svn commit -m "$1"
+
+# create the tag from trunk (let svn create versioned tags/$2)
 svn cp trunk tags/$2
-svn --username shift8 add tags/$2/*
+
+# commit the tag
 svn --username shift8 ci -m "$1 version $2"
+
+# back to original dir (kept exactly as you had)
 cd ../
+
